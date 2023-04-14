e09df5ee30a7d0b57a180b50455bd446031c63fa
swoops503 <james.roundy4@gmail.com>
2023-04-13 18:48:16 -0700

    added 2 media tags for lower resolution platforms, added contact form and js functionality for the form

--- javascript.js
+++ javascript.js
@@ -1,6 +1,33 @@
+// variables from block09 simple js rubric requirement
+
 const var1 = "Hello ";
 const var2 = "from ";
 const var3 = "the ";
 const var4 = "console!";
 
+// output for simple js script
+
 console.log(var1+var2+var3+var4);
+
+//form data collector and form js page alerts
+
+const form = document.querySelector("#form")
+   const submitButton = document.querySelector("#submit")
+   const scriptURL = "https://httpbin.org/post"
+
+   form.addEventListener('submit', e => {
+     submitButton.disabled = true
+     e.preventDefault()
+     let requestBody = new FormData(form)
+     fetch(scriptURL, { method: 'POST', body: requestBody})
+       .then(response => {
+          alert('Success!', response)
+          submitButton.disabled = false
+         })
+       .catch(error => {
+       alert('Error!', error.message)
+         submitButton.disabled = false
+
+       }
+       )
+   })
